4903d2d25fcc28e9661f354fed5fcc9958461bde
SilverousBlack <juliancalebsegundo@gmail.com>
2022-02-07 03:58:19 +0800

    0.0.3d
    Added Release Major Update Target Timeline for March 2022 Release.

--- src/ChaosIntelligence/modules/GraphAssimilationEngine.py
+++ src/ChaosIntelligence/modules/GraphAssimilationEngine.py
@@ -5,4 +5,9 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 import typing
 
-
+class GraphAssimilator():
+    """This object takes record of inputs and results of a data model to create an assimilation model.
+    
+    
+    """
+    pass
