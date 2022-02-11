2c73b38070b0d0f130093e8da74b0fef72755823
SilverousBlack <juliancalebsegundo@gmail.com>
2022-02-12 00:54:02 +0800

    0.0.3d
    Assimilator build protocol now working properly.

--- src/ChaosIntelligence/modules/GraphAssimilationEngine.py
+++ src/ChaosIntelligence/modules/GraphAssimilationEngine.py
@@ -8,9 +8,10 @@ import typing
 class GraphAssimilator():
     pass
 
-def new_call(self, **kwargs):
-    self.assimilator["other"] = kwargs
-    output = self._old_call(**kwargs)
+def __new_call__(self, inputs, *args, **kwargs):
+    self.assimilator["input"] = inputs
+    self.assimilator["other"] = (args, kwargs)
+    output = self._old_call(inputs, *args, **kwargs)
     self.assimilator["output"] = output
     return output
 
@@ -26,9 +27,10 @@ class AssimilatorNode():
             self.revert()
         self.bound_object = TargetLayer
         setattr(TargetLayer, "assimilator", {})
-        TargetLayer._old_call = TargetLayer.call
+        boundmethod = TargetLayer.call.__get__(TargetLayer, TargetLayer.__class__)
+        setattr(TargetLayer, "_old_call", boundmethod)
         TargetLayer.assimilator["tracker"] = self
-        boundmethod = new_call.__get__(TargetLayer, TargetLayer.__class__)
+        boundmethod = __new_call__.__get__(TargetLayer, TargetLayer.__class__)
         setattr(TargetLayer, "call", boundmethod)
         self._location = len(self.tracked_object.assimilators)
     
@@ -79,9 +81,12 @@ class GraphAssimilator():
         self.bound_object = TargetModel
         for layer in TargetModel.layers:
             node = AssimilatorNode(self)
-            node.imbue(layer)
-            self.layers.append(layer)
-            self.assimilators.append(node)
+            if not isinstance(layer, layers.InputLayer):
+                node.imbue(layer)
+                self.layers.append(layer)
+                self.assimilators.append(node)
+            else:
+                continue
     
     def retrieve(self):
         self.map = []
@@ -91,55 +96,3 @@ class GraphAssimilator():
     
     def revert(self):
         pass
-    
-ass = GraphAssimilator()
-inputs = tf.keras.Input(shape=(784,), name="digits")
-x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
-x = layers.Dense(64, activation="relu", name="dense_2")(x)
-outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
-model = tf.keras.Model(inputs=inputs, outputs=outputs)
-ass.build(model)
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-
-# Preprocess the data (these are NumPy arrays)
-x_train = x_train.reshape(60000, 784).astype("float32") / 255
-x_test = x_test.reshape(10000, 784).astype("float32") / 255
-
-y_train = y_train.astype("float32")
-y_test = y_test.astype("float32")
-
-# Reserve 10,000 samples for validation
-x_val = x_train[-10000:]
-y_val = y_train[-10000:]
-x_train = x_train[:-10000]
-y_train = y_train[:-10000]
-model.compile(
-    optimizer=tf.keras.optimizers.RMSprop(),  # Optimizer
-    # Loss function to minimize
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-    # List of metrics to monitor
-    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
-)
-print("Fit model on training data")
-history = model.fit(
-    x_train,
-    y_train,
-    batch_size=64,
-    epochs=2,
-    # We pass some validation for
-    # monitoring validation loss and metrics
-    # at the end of each epoch
-    validation_data=(x_val, y_val),
-)
-# Evaluate the model on the test data using `evaluate`
-print("Evaluate on test data")
-results = model.evaluate(x_test, y_test, batch_size=128)
-print("test loss, test acc:", results)
-
-# Generate predictions (probabilities -- the output of the last layer)
-# on new data using `predict`
-print("Generate predictions for 3 samples")
-predictions = model.predict(x_test[:3])
-print("predictions shape:", predictions.shape)
-ass.retrieve()
-model
